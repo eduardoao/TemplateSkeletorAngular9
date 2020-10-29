419f2e6c8e723156589c6ace9587f1ba4666fddc
Eduardo <eoalcantara@gmail.com>
2020-10-29 18:50:25 -0300

    skeletor angular with bootstrap ok

---
+++ front/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { HomeComponent } from './components/views/home/home.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ front/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+
+import { NavComponent } from './components/template/nav/nav.component';
+import { SidebarComponent } from './components/template/sidebar/sidebar.component';
+import { HomeComponent } from './components/views/home/home.component';
+import { ContentComponent } from './components/views/content/content.component';
+import { LoginComponent } from './components/views/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    NavComponent,
+    SidebarComponent,
+    HomeComponent,
+    ContentComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
